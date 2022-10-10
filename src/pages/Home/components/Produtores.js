cdf1529edde185831b84791e6894e5906a3eddb8
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-10-10 10:27:41 -0300

    FlatList de Produtores

--- src/pages/Home/components/Produtores.js
+++ src/pages/Home/components/Produtores.js
@@ -1,14 +1,37 @@
-import React, {useEffect} from 'react';
-import {Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, Text, StyleSheet} from 'react-native';
 import {getProdutores} from '../../../service/GetData';
 
 function Produtores() {
+  const [titulo, setTitulo] = useState('');
+  const [lista, setLista] = useState([]);
+
   useEffect(() => {
     const response = getProdutores();
-    console.log(response);
+    setTitulo(response.titulo);
+    setLista(response.lista);
   }, []);
 
-  return <Text>Produtores</Text>;
+  const Header = () => {
+    return <Text style={styles.header}>{titulo}</Text>;
+  };
+  return (
+    <FlatList
+      data={lista}
+      renderItem={({item: {nome}}) => <Text>{nome}</Text>}
+      ListHeaderComponent={Header}
+    />
+  );
 }
 
 export default Produtores;
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 20,
+    lineHeight: 32,
+    marginHorizontal: 16,
+    marginTop: 16,
+    fontWeight: 'bold',
+  },
+});
