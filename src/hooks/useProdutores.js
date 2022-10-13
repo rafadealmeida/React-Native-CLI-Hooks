45c115538296e1d3341ab66823567a9e57ff98a7
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-10-13 10:45:51 -0300

    Ordenação

--- src/hooks/useProdutores.js
+++ src/hooks/useProdutores.js
@@ -7,6 +7,9 @@ export default function useProdutores() {
 
   useEffect(() => {
     const response = getProdutores();
+    response.lista.sort(
+      (produtor1, produtor2) => produtor1.distancia - produtor2.distancia,
+    );
     setTitulo(response.titulo);
     setLista(response.lista);
   }, []);
